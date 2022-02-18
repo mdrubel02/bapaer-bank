5e01fbe248aed457f20d9c85abf8ec4381142cab
mdrubel02 <mdrubel072125@gmail.com>
2022-02-18 20:46:27 +0600

    added

--- baper-bank-js-file/account.js
+++ baper-bank-js-file/account.js
@@ -13,11 +13,28 @@ function updateTotalValue(fildId , amount){
     const prevousTotal=parseFloat(prevousTotalText);//innnerText er man ta ke shokhai rupantor korsi
     const newTotal=prevousTotal + amount; // ager line er je shokha ta paisi oita shte perimater diya je value ta asbe oitar shte plus hobe
     totalTag.innerText=newTotal; //eita duita mile ja plus hoilo ei maan ta set kore dilam 
+    return newTotal; // eikhn thke new total er maan ta niya jabe
 }
+//totalupdate function 
+function updateBalance(amount){
+    const balanceTag=document.getElementById('main-balance-Box');
+    const balanceText=balanceTag.innerText;
+    const prevousBalance=parseFloat(balanceText);
+    const newBalance=prevousBalance + amount
+    balanceTag.innerText=newBalance;
+}
+
 // handler deposit button 
 document.getElementById('deposit-button').addEventListener('click', function(){
-    console.log('btn clicked')
     const amount=getInputValue('deposit-amount'); //getinputvalue function er maan ta amount variable er modhe rakha hoise
     updateTotalValue('deposit-Box',amount); // eikhne function ta ke call kore argument hisabe id and ager variable er amount maan ta pathanu hoise
-    
+    updateBalance(amount);
+});
+
+
+
+//handle withdraw button
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const number=getInputValue('withdraw-amount'); //eita function ke call korar pore input er maan ta antese
+    updateTotalValue('withdraw-Box', number); // eikhne function ta ke call kore argument hisabe id and ager variable er amount maan ta pathanu hoise
 })
