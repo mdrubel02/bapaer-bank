e140b0bf75af05509d4799c8532e01566f5476ca
mdrubel02 <mdrubel072125@gmail.com>
2022-02-18 23:25:30 +0600

    added

--- baper-bank-js-file/login.js
+++ baper-bank-js-file/login.js
@@ -7,7 +7,7 @@ document.getElementById('login-button').addEventListener('click', function(){
     const inputPasswordText=inputPassword.value;
     console.log(inputPasswordText);
     if(inputEmailText == 'mdibrahim529907@gmail.com' && inputPasswordText=='Rubel231947'){
-        window.location.href='http://127.0.0.1:5500/bapaer-bank-html-file/account.html';
+        window.location.href="http://127.0.0.1:5500/bapaer-bank-html-file/account.html";
     }
     else{
         alert('please valid informaiton');
